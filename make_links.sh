d4cb13de0cc475bf2a44f2d1863c623b7e123b30
lukschwalb <schwalb@luk.im>
2020-05-28 12:04:57 +0200

    Whoopsie

--- make_links.sh
+++ make_links.sh
@@ -2,5 +2,5 @@
 
 dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
-ln -s $dir/.tmux.conf ~/.tmux.conf
-ln -s $dir/.vimrc ~/.vimrc
+ln -s $dir/tmux.conf ~/.tmux.conf
+ln -s $dir/vimrc ~/.vimrc
